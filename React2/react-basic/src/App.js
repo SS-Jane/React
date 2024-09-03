748259b1fbfb756dceb9970a5790def70c959050
SS-Jane <superjane.prodev@gmail.com>
2024-09-03 22:12:27 +0700

    add react basic 2

---
+++ React2/react-basic/src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Money report</h1>
+      <p>Payment dialy report</p>
+      <ul>
+        <li>
+          Transport <span>-200</span>THB
+        </li>
+        <li>
+          Salary <span>+20,000</span>THB
+        </li>
+        <li>
+          Food<span>-100</span>THB
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ React2/react-basic/src/Component/HelloComponent.jsx
@@ -0,0 +1,7 @@
+export default function HelloComponent() {
+  return (
+    <div>
+      <h1>Hello Component Extaernal</h1>
+    </div>
+  );
+}
