105005ef6b2dc5a08446833af654458aa2d0a6f3
SS-Jane <superjane.prodev@gmail.com>
2024-09-06 10:10:51 +0700

    ad react basic 3

--- React2/react-basic/src/Component/Item.jsx
+++ React2/react-basic/src/Component/Item.jsx
@@ -1,16 +1,20 @@
-export default function Item() {
+import PropTypes from 'prop-types';
 
-    const item = 'Rent';
-    const cost = '2500'
+
+export default function Item(props) {
+  const { title, amount } = props;
 
   return (
     <>
       <li className="bg-white shadow-md shadow-black/15 p-3 m-1 flex justify-between">
-        {item}<span> {cost} </span> THB
+        {title}
+        <span>{amount} THB</span>
       </li>
     </>
   );
 }
 
-
-//className="bg-white shadow-md shadow-black/15 p-15 m-1 flex justify-between
+Item.propTypes ={
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired
+}
--- React2/react-basic/src/Component/Transaction.jsx
+++ React2/react-basic/src/Component/Transaction.jsx
@@ -1,14 +1,20 @@
 import Item from "./Item";
+import { v4 as uuidv4 } from "uuid"; // auto generate ID
 // import "./Transaction.css";
+const data = [
+  { title: "Car maintain", amount: 20000 },
+  { title: "Salary", amount: 50000 },
+  { title: "Food/Beverage", amount: 3000 },
+  { title: "Rental", amount: 2500 },
+  { title: "Credit/Loan", amount: 7500 },
+];
 
 export default function Transaction() {
   return (
     <div className="item-list list-none p-0 mb-40">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+      {data.map((e) => {
+        return <Item key={uuidv4()} {...e} />;
+      })}
     </div>
   );
 }
